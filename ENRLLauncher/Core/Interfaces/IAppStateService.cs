2f94b361582691a030f52144e7a67ccee0845705
Isaac <ibroomall@arizona.edu>
2026-09-14 08:23:14 -0700

    Should be versioned

---
+++ ENRLLauncher/Core/Interfaces/IAppStateService.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using ENRLLauncher.Core.Enums;
+
+namespace ENRLLauncher.Core.Interfaces;
+
+public interface IAppStateService : INotifyPropertyChanged
+{
+    bool IsEditMode { get; set; }
+    bool IsHomeViewActive { get; set; }
+
+    event Action<LaunchTargetType>? AddSeparatorRequested;
+
+    void RequestAddSeparator(LaunchTargetType targetType);
+}
---
+++ ENRLLauncher/Core/Services/AppStateService.cs
@@ -0,0 +1,43 @@
+﻿using ENRLLauncher.Core.Enums;
+using ENRLLauncher.Core.Interfaces;
+using ENRLLauncher.Core.Utilities;
+
+namespace ENRLLauncher.Core.Services;
+
+public class AppStateService : ObservableObject, IAppStateService
+{
+    private bool _isEditMode;
+    private bool _isHomeViewActive = true;
+
+    public bool IsEditMode
+    {
+        get => _isEditMode;
+        set
+        {
+            var targetValue = value && _isHomeViewActive;
+            Set(ref _isEditMode, targetValue);
+        }
+    }
+
+    public bool IsHomeViewActive
+    {
+        get => _isHomeViewActive;
+        set
+        {
+            if (Set(ref _isHomeViewActive, value) && !_isHomeViewActive && _isEditMode)
+            {
+                IsEditMode = false;
+            }
+        }
+    }
+
+    public event Action<LaunchTargetType>? AddSeparatorRequested;
+
+    public void RequestAddSeparator(LaunchTargetType targetType)
+    {
+        if (IsEditMode && IsHomeViewActive)
+        {
+            AddSeparatorRequested?.Invoke(targetType);
+        }
+    }
+}
